865d11d2a35f549c4012b923eb7a7ceff07626f4
Harsh Gupta <92781391+itachi-x7@users.noreply.github.com>
2024-07-22 17:15:54 +0530

    conn.java

--- conn.java
+++ conn.java
@@ -5,25 +5,29 @@
  */
 package hotel.management.system;
 
-
-
-
+// Import necessary packages for SQL operations
 import java.sql.*;  
 
-public class conn{
-    Connection c;
-    Statement s;
-    public conn(){  
-        try{  
+// Define a class named `conn` to manage the database connection
+public class conn {
+    Connection c;  // Declare a Connection object to establish a connection to the database
+    Statement s;   // Declare a Statement object to execute SQL queries
+
+    // Constructor of the `conn` class
+    public conn() {  
+        try {  
+            // Load the MySQL JDBC driver class dynamically at runtime
             Class.forName("com.mysql.jdbc.Driver");  
-            c =DriverManager.getConnection("jdbc:mysql:///hms","root",""); 
             
-            s =c.createStatement();  
+            // Establish a connection to the database named 'hms' using the username 'root' and no password
+            c = DriverManager.getConnection("jdbc:mysql:///hms", "root", ""); 
             
-           
-        }catch(Exception e){ 
+            // Create a Statement object to execute SQL queries
+            s = c.createStatement();  
+            
+        } catch(Exception e) { 
+            // Print any exception that occurs to the console
             System.out.println(e);
         }  
     }  
 }  
-
